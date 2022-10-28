73da7a432fd0ac8ede691970a1788a741eb78a4d
AbhayBhan <abhaybhan197@gmail.com>
2022-10-28 14:26:36 +0530

    Developed the website to a good start

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
       lg: '1080px',
       xl: '1440px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        BrightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        DarkBlue: 'hsl(228, 39%, 23%)',
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)',
+        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        VeryPaleRed: 'hsl(13, 100%, 96%)',
+        VeryLightGray: 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   plugins: [],
 }
